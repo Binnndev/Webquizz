0203045cd30856fdee4aeecd385670c7c2b6208a
Trần Hoàng Hải ( Bin Dev ) <92655443+Binnndev@users.noreply.github.com>
2025-06-16 11:14:04 +0700

    fix DB
    fix DB

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 require("dotenv/config");
 const DBConnection = require("./database/DBConnection");
 const express = require("express");
-const app = express();
 const path = require("path");
 const cors = require("cors");
 
 DBConnection.dbConnect();
 
+const app = express();
 app.use(
   cors({
+    origin: "http://localhost:3000",
     exposedHeaders: ["auth-token"],
   })
 );
+
 app.use(express.json());
 
 // serving static files from react
@@ -21,11 +23,16 @@ app.use(express.static(path.join(__dirname, "client", "build")));
 const routes = require("./app/router");
 
 // route middleware
-app.use(routes);
+app.use(process.env.API_URI, routes);
 
 // fallback for react routes
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "", "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something broke!" });
 });
 
 app.listen(process.env.PORT || 5000, () =>
